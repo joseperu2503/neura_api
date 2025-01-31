09a78be7a269ab9a7b5080d03c6ffd65739597a4
Jose Perez <joseperu2503@gmail.com>
2025-01-30 21:27:30 -0500

    register

--- src/auth/dto/register.dto.ts
+++ src/auth/dto/register.dto.ts
@@ -1,10 +1,25 @@
-import { IsEmail, IsString, MinLength } from 'class-validator';
+import { IsEmail, IsString, MinLength, Matches } from 'class-validator';
+import { Match } from '../decorators/match.decorator';
 
 export class RegisterDto {
   @IsEmail()
   email: string;
 
   @IsString()
-  @MinLength(6)
+  @MinLength(6, { message: 'Password must be at least 6 characters long' })
+  @Matches(/(?=.*[a-z])/, {
+    message: 'Password must contain at least one lowercase letter',
+  })
+  @Matches(/(?=.*[A-Z])/, {
+    message: 'Password must contain at least one uppercase letter',
+  })
+  @Matches(/(?=.*\d)/, { message: 'Password must contain at least one number' })
+  @Matches(/(?=.*[@$!%*?&])/, {
+    message: 'Password must contain at least one special character (@$!%*?&)',
+  })
   password: string;
+
+  @IsString()
+  @Match('password', { message: 'Passwords do not match' })
+  passwordConfirmation: string;
 }
